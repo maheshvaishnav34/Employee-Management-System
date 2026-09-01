80b39e3b1b0cabe813349f0f7f8c900d6f229011
Mahesh Vaishnav <yourgithubemail@gmail.com>
2026-09-01 22:16:50 +0530

    Ensure /api path prefix normalization in Vercel serverless entrypoint

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
 const app = require('../backend/server');
 
-module.exports = app;
+module.exports = (req, res) => {
+  if (req.url && !req.url.startsWith('/api')) {
+    req.url = '/api' + req.url;
+  }
+  return app(req, res);
+};
